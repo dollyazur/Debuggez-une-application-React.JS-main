7497e4109e1a4998827d5263e1fe74093eafdd63
dollyazur <azurgrisby@hotmail.fr>
2025-01-06 17:34:46 +0100

    correction affichage des mois, mais ne correspond pas à la période des modalevent...

--- src/components/EventCard/index.js
+++ src/components/EventCard/index.js
@@ -6,27 +6,40 @@ import "./style.scss";
 const EventCard = ({
   imageSrc,
   imageAlt,
-  date = new Date(),
+  date = new Date(), // date par défaut en attendant d'en avoir une
   title,
   label,
   small = false,
   ...props
-}) => (
+}) => {
+  // Ici, on vérifie que la date est une vraie date magique
+  const validDate = date instanceof Date ? date : new Date(date);
+
+  return (
     <div
       data-testid="card-testid"
       className={`EventCard${small ? " EventCard--small" : ""}`}
       {...props}
     >
-      <div className="EventCard__imageContainer" id="nos-realisations">
+
+      {/* Image de la carte */}
+      <div className="EventCard__imageContainer" id="">
         <img data-testid="card-image-testid" src={imageSrc} alt={imageAlt} />
         <div className="EventCard__label">{label}</div>
       </div>
+
+      {/* Description de la carte */}
       <div className="EventCard__descriptionContainer">
         <div className="EventCard__title">{title}</div>
-        <div className="EventCard__month">{getMonth(date)}</div>
+
+
+        {/* On utilise `getMonth` pour afficher le mois correct */}
+        <div className="EventCard__month">{getMonth(validDate)}</div>
       </div>
     </div>
   );
+};
+
 
 EventCard.propTypes = {
   imageSrc: PropTypes.string.isRequired,
--- src/helpers/Date/index.js
+++ src/helpers/Date/index.js
@@ -13,4 +13,4 @@ export const MONTHS = {
   12: "décembre",
 };
 
-export const getMonth = (date) => MONTHS[date.getMonth()];
+export const getMonth = (date) => MONTHS[date.getMonth()+1]; // démarrage à 0 pas à 1!!
